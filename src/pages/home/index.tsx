09ac2f535fcc0d4a1e4aaa04e54785a1ab3426f4
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2023-07-01 21:13:08 -0300

    wip: product example

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -12,6 +12,85 @@ export default function Home() {
         console.log(user)
     }, [user, isLoading])
 
+    const mockProducts = [
+        {
+            id: "7d30bd81-a84a-4348-982c-5cda7e4eb48e",
+            createdAt: "2023-07-02T01:25:53.377Z",
+            updatedAt: "2023-07-02T01:25:53.377Z",
+            deletedAt: null,
+            name: "Corsa",
+            value: 5000,
+            category: "BOOKS",
+            description: "",
+            status: true,
+            photos: [
+              {
+                id: "89908be3-46dc-402d-a687-f1a68fe95971",
+                createdAt: "2023-07-02T01:25:53.351Z",
+                updatedAt: "2023-07-02T01:25:53.000Z",
+                deletedAt: null,
+                photo_url: "https://quatrorodas.abril.com.br/wp-content/uploads/2021/04/Corsa-Wind-modelo-1997-da-Chevrolet._1-C%C3%B3pia.jpg"
+              },
+              {
+                id: "89908be3-46dc-402d-a687-f1a68fe95971",
+                createdAt: "2023-07-02T01:25:53.351Z",
+                updatedAt: "2023-07-02T01:25:53.000Z",
+                deletedAt: null,
+                photo_url: "https://www.carrosyclasicos.com/media/k2/items/cache/60468aa82473fd32700d48478784d483_XL.jpg"
+              }
+            ],
+            owner: {
+              id: "09a4d9df-d464-41d5-a9ec-7ded1ae26086",
+              createdAt: "2023-06-27T16:04:31.541Z",
+              updatedAt: "2023-06-27T16:04:31.541Z",
+              deletedAt: null,
+              full_name: "Vitoria Sabrina",
+              state: "SC",
+              city: "Joinville",
+              email: "vitoria@email.com",
+              password: "$2b$08$tWARwTylWe30DYhKVfIBdeavIO/x1tR0lieD9FwSxQa2307VyuyAu"
+            }
+          },
+          {
+            id: "7d30bd81-a84a-4348-982c-5cda7e4eb48e",
+            createdAt: "2023-07-02T01:25:53.377Z",
+            updatedAt: "2023-07-02T01:25:53.377Z",
+            deletedAt: null,
+            name: "Astra",
+            value: 5000,
+            category: "BOOKS",
+            description: "",
+            status: true,
+            photos: [
+              {
+                id: "89908be3-46dc-402d-a687-f1a68fe95971",
+                createdAt: "2023-07-02T01:25:53.351Z",
+                updatedAt: "2023-07-02T01:25:53.000Z",
+                deletedAt: null,
+                photo_url: "https://quatrorodas.abril.com.br/wp-content/uploads/2021/04/Corsa-Wind-modelo-1997-da-Chevrolet._1-C%C3%B3pia.jpg"
+              },
+              {
+                id: "89908be3-46dc-402d-a687-f1a68fe95971",
+                createdAt: "2023-07-02T01:25:53.351Z",
+                updatedAt: "2023-07-02T01:25:53.000Z",
+                deletedAt: null,
+                photo_url: "https://www.carrosyclasicos.com/media/k2/items/cache/60468aa82473fd32700d48478784d483_XL.jpg"
+              }
+            ],
+            owner: {
+              id: "09a4d9df-d464-41d5-a9ec-7ded1ae26086",
+              createdAt: "2023-06-27T16:04:31.541Z",
+              updatedAt: "2023-06-27T16:04:31.541Z",
+              deletedAt: null,
+              full_name: "Vitoria Sabrina",
+              state: "SC",
+              city: "Joinville",
+              email: "vitoria@email.com",
+              password: "$2b$08$tWARwTylWe30DYhKVfIBdeavIO/x1tR0lieD9FwSxQa2307VyuyAu"
+            }
+          }
+    ]
+
     return (
         <> 
             {!isLoading && user && (
@@ -32,7 +111,14 @@ export default function Home() {
                         <a className="bg-white rounded-full w-16 h-16 flex-none" href=""></a>
                     </div>
                 </div>
-                <div className="bg-white h-screen w-3/4"></div>
+                <div className="bg-white h-screen w-3/4">
+                    {mockProducts?.map((product) => (
+                        <div>
+                            <p>{product.name}</p>
+                            <img src={product.photos[0].photo_url} alt="" />
+                        </div>
+                    ))}
+                </div>
             </div>
             )}
         </>
