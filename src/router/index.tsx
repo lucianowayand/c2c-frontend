411b06c3e8bfb88d390495bbf2baf4da402fe9bb
Lucas-Tascheck <lucasguitascheck@gmail.com>
2023-07-02 11:54:07 -0300

    feat: product page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/login";
 import NotFound from "../pages/404";
 import Home from "../pages/home";
+import ProductPage from "../pages/productPage";
 
 export default function Router() {
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/productPage" element={<ProductPage />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     </BrowserRouter>
