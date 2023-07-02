f61a34c7e18196314ff19ee2434a85d284838d25
Lucas-Tascheck <lucasguitascheck@gmail.com>
2023-07-02 12:46:46 -0300

    feat: publish product page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,6 +4,7 @@ import Login from "../pages/login";
 import NotFound from "../pages/404";
 import Home from "../pages/home";
 import ProductPage from "../pages/productPage";
+import PublishProductPage from '../pages/publishProductPage'
 
 export default function Router() {
     return <BrowserRouter>
@@ -11,6 +12,7 @@ export default function Router() {
             <Route path="/" element={<Login />} />
             <Route path="/home" element={<Home />} />
             <Route path="/productPage" element={<ProductPage />} />
+            <Route path="/publishProductPage" element={<PublishProductPage />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     </BrowserRouter>
