3dfbba04280a71cef7de5d433d93e7a9c0368383
Lucas-Tascheck <lucasguitascheck@gmail.com>
2023-07-03 22:26:09 -0300

    feat: dropdown menu

--- src/components/dashboard-layout/index.tsx
+++ src/components/dashboard-layout/index.tsx
@@ -4,6 +4,7 @@ import { useAuth } from "../../context/AuthContext"
 export default function DashboardLayout({ children }: any) {
     const { user, isLoading } = useAuth()
     const [productName, setProductName] = useState('')
+    const [dropdownMenu, setDropdownMenu] = useState(false)
 
     useEffect(() => {
         if(isLoading) return
@@ -18,11 +19,16 @@ export default function DashboardLayout({ children }: any) {
         {!isLoading && user && (
             <div className=" flex flex-col h-screen bg-gray-300">
                 <div className="bg-orange-600 h-24 w-screen flex flex-col items-center">
-                    <a href="" className=' absolute left-6 mt-6 flex flex-col gap-2 w-12 h-12'>
+                    <div className=' absolute left-6 mt-6 flex flex-col gap-2 w-12 h-12' onClick={() => setDropdownMenu(!dropdownMenu)}>
                         <div className='w-full h-2 bg-white'></div>
                         <div className='w-full h-2 bg-white'></div>
                         <div className='w-full h-2 bg-white'></div>
-                    </a>
+                    </div>
+                    {dropdownMenu && <div className="absolute flex flex-col bg-zinc-200 w-40 left-0 ml-20 mt-20 space-y-px">
+                        <a href='' className="bg-zinc-100 p-1">Meus anúncios</a>    
+                        <a href='' className="bg-zinc-100 p-1">Chat</a>  
+                        <a href='' className="bg-zinc-100 p-1">Logout</a>  
+                    </div>}
                     <div className="w-1/3 flex justify-center">
                         <input className="w-96 h-12 mt-6 flex-none" type="text" value={productName} onChange={(e) => setProductName(e.target.value)}/>
                         <a className="h-12 w-12 bg-white mt-6 ml-2 flex-none" href={`/home?product-name=${productName}`}></a>
