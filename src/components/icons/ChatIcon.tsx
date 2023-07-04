3b6c93be97cae23734d61ca04daffb2cba90bf50
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2023-07-03 21:36:23 -0300

    feat: start conversation

---
+++ src/components/icons/ChatIcon.tsx
@@ -0,0 +1,3 @@
+export default function ChatIcon({size=24, color="#ffffff"}){
+    return <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-message-circle"><path d="m3 21 1.9-5.7a8.5 8.5 0 1 1 3.8 3.8z"/></svg>
+}
--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -6,13 +6,16 @@ import ArrowRight from '../../components/icons/ArrowRight'
 import moment from 'moment'
 import { useParams } from 'react-router-dom'
 import { api } from '../../services/api'
+import ChatIcon from '../../components/icons/ChatIcon'
+import { useAuth } from '../../context/AuthContext'
 
 export default function ProductPage() {
     const {product_id} = useParams()
+    const {user} = useAuth()
     const [product, setProduct]= useState<any>()
 
     useEffect(() => {
-    getProductInfo()
+      getProductInfo()
     }, [])
 
     async function getProductInfo() {
@@ -24,48 +27,22 @@ export default function ProductPage() {
       }
     }
 
-    const mockProduct = {
-        id: "7d30bd81-a84a-4348-982c-5cda7e4eb48e",
-        createdAt: "2023-07-02T01:25:53.377Z",
-        updatedAt: "2023-07-02T01:25:53.377Z",
-        deletedAt: null,
-        name: "Corsa",
-        value: 5000,
-        category: "BOOKS",
-        description: "Vendo carro usado",
-        status: true,
-        photos: [
-          {
-            id: "89908be3-46dc-402d-a687-f1a68fe95971",
-            createdAt: "2023-07-02T01:25:53.351Z",
-            updatedAt: "2023-07-02T01:25:53.000Z",
-            deletedAt: null,
-            photo_url: "https://quatrorodas.abril.com.br/wp-content/uploads/2021/04/Corsa-Wind-modelo-1997-da-Chevrolet._1-C%C3%B3pia.jpg"
-          },
-          {
-            id: "89908be3-46dc-402d-a687-f1a68fe95971",
-            createdAt: "2023-07-02T01:25:53.351Z",
-            updatedAt: "2023-07-02T01:25:53.000Z",
-            deletedAt: null,
-            photo_url: "https://www.carrosyclasicos.com/media/k2/items/cache/60468aa82473fd32700d48478784d483_XL.jpg"
-          }
-        ],
-        owner: {
-          id: "09a4d9df-d464-41d5-a9ec-7ded1ae26086",
-          createdAt: "2023-06-27T16:04:31.541Z",
-          updatedAt: "2023-06-27T16:04:31.541Z",
-          deletedAt: null,
-          full_name: "Vitoria Sabrina",
-          state: "SC",
-          city: "Joinville",
-          email: "vitoria@email.com",
-          password: "$2b$08$tWARwTylWe30DYhKVfIBdeavIO/x1tR0lieD9FwSxQa2307VyuyAu"
+    async function createChat() {
+      try{
+        const res = await api.post(`/api/v1/products/${product_id}/chat/${user?.id}`)
+        const chat = res.data
+        if (chat) {
+          window.location.href = `/chat/${chat.id}`
         }
+      } catch (error) {
+        console.log(error)
       }
-      
+
+    }
+
     const [photoIndex, setPhotoIndex] = useState(0) 
-    const productDate = moment(mockProduct.owner.createdAt).format("DD/MM/YYYY");
-    const publishedAt = moment(mockProduct.createdAt).format("DD/MM/YYYY") + " às " + moment(mockProduct.createdAt).format("HH:mm");
+    const productDate = moment(product?.owner?.createdAt).format("DD/MM/YYYY");
+    const publishedAt = moment(product?.createdAt).format("DD/MM/YYYY") + " às " + moment(product?.createdAt).format("HH:mm");
     return (
         <DashboardLayout>
           <div className="flex w-full justify-center p-8 space-x-24 mt-12">
@@ -106,9 +83,11 @@ export default function ProductPage() {
                     <div className="bg-zinc-500 w-full h-px mt-16"></div>
                 </div>
                 <div className="self-center font-semibold">Membro desde: {productDate}</div>
-                <div className="self-center w-1/2 h-12 bg-orange-600 rounded-full flex items-center justify-center gap-3 mt-8">
-                    <img className='' src={chatIcon} alt="" />
-                    <div className="text-white text-xl">Chat</div>
+                <div className="self-center w-1/2 h-12 bg-orange-600 rounded-full flex items-center justify-center gap-3 mt-8 cursor-pointer"
+                  onClick={createChat}
+                >
+                  <ChatIcon />
+                  <div className="text-white text-xl">Chat</div>
                 </div>
             </div>
             </>
